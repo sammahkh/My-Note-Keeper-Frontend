6f22232071ba72f1b0368449a5a89905b3dee6f4
Samah Bani Odeh <110301925+sammahkh@users.noreply.github.com>
2025-05-05 17:04:26 +0000

    Implement Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  return <></>;
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <div className={`${darkMode && "dark-mode"}`}>
+      <div className='container'>
+      <Header handleToggleDarkMode={setDarkMode} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
